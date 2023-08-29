d975a9219b72ff43a8190711031989a56644c2d1
Benjamin Smidt <benjamin.smidt@utexas.edu>
2023-08-28 21:26:17 -0500

    finish through part 5 of pset 1

---
+++ res6-007/01-intro/prob4.py
@@ -0,0 +1,49 @@
+"""
+Author: Benjamin Smidt
+Course: RES.6-007: Signals and Systems
+Professor: Alan V. Oppenheim
+Date: 08/28/2020
+Assignment: Problem Set 1, Problem 4
+"""
+import numpy as np
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+
+def main() -> None:
+    r = 2/3
+    theta = np.pi/6
+
+    radii = [r, r**2, r, r**2, r**2, r]
+    thetas = [-theta, 2*theta, theta+np.pi/2, 0, 2*theta, -theta]
+    colors = ['r', 'g', 'b', 'y', 'm', 'c']
+    labels = ['i', 'ii', 'iii', 'iv', 'v', 'vi']
+
+    # create polar plot
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+
+    # plot coordinates as vectors
+    for i in range(len(radii)):
+        arrowprops = dict(arrowstyle="->", linewidth=2, color=colors[i])
+        ax.annotate(
+            '', xy=(thetas[i], radii[i]), xytext=(0, 0), arrowprops=arrowprops
+        )
+
+    # Add a legend
+    legend_handles = []
+    for i in range(len(colors)):
+        legend_handles.append(
+            mpatches.Patch(color=colors[i], label=labels[i])
+        )
+    ax.legend(handles=legend_handles, title='Vectors')
+    # other graph settings
+    ax.set_rmax(max(radii))
+    ax.set_rticks(np.arange(0, r, 1/9))
+    ax.set_rlabel_position(-22.5)
+    ax.grid(True)
+    ax.set_title("Problem 4")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
